d3f50ff37b7b22cfc452dcb4f71598c8a01bfd18
Elizaveta <154224117+SparrowEl@users.noreply.github.com>
2024-02-09 21:03:12 +0300

    Update dict_bot.py
    Удалила токен

--- Бот-словарик/dict_bot.py
+++ Бот-словарик/dict_bot.py
@@ -5,7 +5,7 @@
 
 from telebot import TeleBot, types
 
-bot = TeleBot(token='6967453423:AAEGNoIKqK2N0olD-zBvu-nF7Sj9clL0Cxg', parse_mode='html') # создание бота
+bot = TeleBot(token='ghghjgjgjgjgjgjggj', parse_mode='html') # создание бота
 
 # словарь с определениями и аббревиатурами, которые знает бот
 # в формате:
